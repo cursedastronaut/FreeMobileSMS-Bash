5ea6d7e223995349d19b3d5fb32e9221d4bb01b1
gmarti13 <galaad.martineaux@universite-paris-saclay.fr>
2024-01-26 21:59:11 +0100

    Changed key filenames.

--- sms.sh
+++ sms.sh
@@ -4,8 +4,8 @@ MESSAGE="what_you_want_to_send" #Make sure this supports URL syntax
 CURL=/usr/bin/curl
 URL=https://smsapi.free-mobile.fr/sendmsg
 
-PASS=$(cat sms.key)
-USER=$(cat user.key)
+PASS=$(cat password.key)
+USER=$(cat userid.key)
 
 
 $CURL -k -X POST "https://smsapi.free-mobile.fr/sendmsg?user=$USER&pass=$PASS&msg=$MESSAGE"
